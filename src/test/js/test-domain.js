2f8c7a44064421f1b1ce50d7ecf64660e967cbe8
Pawel Pelczar <ppelczar@pgs-soft.com>
2012-03-04 14:31:46 +0100

    Added with in unit tests

--- src/test/js/test-domain.js
+++ src/test/js/test-domain.js
@@ -3,8 +3,10 @@ $(document).ready(function(){
   module("Domain");
 
   test("Timeslot object", function() {
-    var timeslot = new pl.com.paweusz.schedule.domain.Timeslot(1, 2);
-    equal(timeslot.getDuration(), 1, "Duration should be 1");
+    with (pl.com.paweusz.schedule.domain) {
+      var timeslot = new Timeslot(1, 2);
+      equal(timeslot.getDuration(), 1, "Duration should be 1");
+    }
   });
 
 });
