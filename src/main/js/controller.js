fe416bd115492a532104650e187d0d71b947ca28
Pawel Pelczar <paweusz@gmail.com>
2012-03-26 19:06:11 +0200

    Displaying timeslots for weekdays

--- src/main/js/controller.js
+++ src/main/js/controller.js
@@ -4,13 +4,16 @@ Schedule.ScheduleController = Ember.Object.extend({
   weekdays: undefined,
   schedule: undefined,
   name: undefined,
+  timeslots: undefined,
   
   init: function() {
     var model = Schedule.Model.create();
     this.schedule = model.loadSchedule("test");
     this.weekdays = this.schedule.getWeekdays();
     this.name = this.schedule.getName();
+    this.timeslots = this.schedule.getTimeslots();
   }
+
 });
 
 Schedule.WeekdaysController = Ember.ArrayController.extend({
--- src/main/js/view.js
+++ src/main/js/view.js
@@ -3,7 +3,8 @@
 Schedule.WeekdayView = Ember.View.extend({
   templateName: 'weekday',
   weekday: undefined,
-  lessons: undefined
+  lessons: undefined,
+  timeslotsBinding: 'Schedule.scheduleController.timeslots'
 });
 
 Schedule.ScheduleView = Ember.View.extend({
