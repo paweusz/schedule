86a0c4d38041ae563382e12c0c0e4d335f4947e6
Pawel Pelczar <paweusz@gmail.com>
2012-04-07 14:46:23 +0200

    Introduced router

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -4,16 +4,7 @@ var Schedule = {};
 
 $(document).ready(function(){
 
-  Schedule.app = {
-    initialize: function() {
-      this.schedule = Schedule.scheduleFactory.createSchedule('ppelczar');
-      this.scheduleView = new Schedule.ScheduleView({
-        el: $("#container"),
-        model: this.schedule
-      });
-      this.scheduleView.render();
-    }
-  };
-
-  Schedule.app.initialize();
+  var router = new Schedule.Router();
+  Backbone.history.start();
+  
 });
--- src/main/js/router.js
+++ src/main/js/router.js
@@ -1,5 +1,23 @@
 "use strict";
 
+Schedule.Router = Backbone.Router.extend({
+
+  routes: {
+    "*action": "displaySchedule"
+  },
+  
+  displaySchedule: function(action) {
+    var schedule = Schedule.scheduleFactory.createSchedule('ppelczar');
+    var scheduleView = new Schedule.ScheduleView({
+      el: $("#container"),
+      model: schedule
+    });
+    scheduleView.render();
+  }
+
+});
+
+/*
 Schedule.ScheduleController = Ember.Object.extend({
   weekdays: null,
   timeslots: null,
@@ -70,5 +88,5 @@ Schedule.ScheduleController = Ember.Object.extend({
   }
 
 });
-
+*/
 
